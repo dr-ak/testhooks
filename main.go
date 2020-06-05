cb359c53b69be7bed35d92e6116456a35f82e693
dr-ka <alex-r-1982@mail.ru>
2020-06-05 14:17:28 +0700

    Global changes

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"os/user"
 	"strings"
 
 	"github.com/gorilla/mux"
@@ -22,78 +23,255 @@ import (
 
 var params Params
 
-type hook struct {
-	From string
-	To   string
-}
-
 type Params struct {
-	Port            string
-	Hooks           []hook
-	Root            string
-	RequestBodyPath string
-	keyHook         int
-	eventType       string
+	Port  string
+	Hooks []hook
 }
 
-func getParams() (out Params) {
-	jsonData, err := ioutil.ReadFile("config.json")
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	err = json.Unmarshal(jsonData, &out)
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
+type hook struct {
+	FromUrl         string
+	HookType        string
+	ToUrl           string
+	Address         string
+	Port            string
+	User            string
+	BaseDir         string
+	Job             string
+	Requestbodypath string
+}
+
+func (h hook) getFromUrl() string {
+	return h.FromUrl
+}
+
+func (h hook) getType() string {
+	return h.HookType
+}
+
+func (h hook) getToUrl() string {
+	return h.ToUrl
+}
+
+func (h hook) getAddress() string {
+	return h.Address
+}
+
+func (h hook) getPort() string {
+	return h.Port
+}
+
+func (h hook) getUser() string {
+	return h.User
+}
+
+func (h hook) getBaseDir() string {
+	return h.BaseDir
+}
+
+func (h hook) getJob() string {
+	return h.Job
+}
+
+func (h hook) getRequestBodyPath() string {
+	return h.Requestbodypath
+}
+
+func (p Params) getHook(url string) (out *hook) {
+	out = nil
+	for _, hook := range p.Hooks {
+		if hook.getFromUrl() == url {
+			return &hook
+		}
 	}
 	return out
 }
 
-func (p Params) getJobName() string {
-	url := p.Hooks[p.keyHook].From[0:strings.Index(p.Hooks[p.keyHook].From, "?")]
-	return url[strings.LastIndex(url, "/")+1:]
+func moveFileOnSsh(h *hook, from, to string) {
+	session, err := getSshSession(h)
+	defer session.Close()
+	if err != nil {
+		writeLog("logs/error.log", err.Error())
+	}
+	fmt.Println(from, to)
+	err = scp.CopyPath(from, to, session)
+	if err != nil {
+		writeLog("logs/error.log", err.Error())
+	}
+	err = os.Remove(from)
+	if err != nil {
+		writeLog("logs/error.log", err.Error())
+	}
 }
 
-func (p Params) getRequestBodyPath() string {
-	return strings.Replace(p.RequestBodyPath, "$job", p.getJobName(), -1)
+func getClientConfig(h *hook) *ssh.ClientConfig {
+	user, err := user.Current()
+	if err != nil {
+		writeLog("logs/error.log", err.Error())
+	}
+	key, err := ioutil.ReadFile(user.HomeDir + "/.ssh/id_rsa")
+	if err != nil {
+		writeLog("logs/error.log", err.Error())
+	}
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		writeLog("logs/error.log", err.Error())
+	}
+	hostKeyCallback, err := knownhosts.New(user.HomeDir + "/.ssh/known_hosts")
+	if err != nil {
+		writeLog("logs/error.log", err.Error())
+	}
+	return &ssh.ClientConfig{
+		User: h.getUser(),
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: hostKeyCallback,
+	}
 }
 
-func (p Params) getHookTo() string {
-	return p.Hooks[p.keyHook].To
+func getSshSession(h *hook) (*ssh.Session, error) {
+	client, err := ssh.Dial("tcp", h.getAddress()+":"+h.getPort(), getClientConfig(h))
+	if err != nil {
+		writeLog("logs/error.log", err.Error())
+	}
+	session, err := client.NewSession()
+	return session, err
 }
 
-func (p *Params) setEvenType(eventType string) {
-	p.eventType = eventType
-}
-
-func (p *Params) setKeyHook(url string) bool {
-	for key, hook := range p.Hooks {
-		if hook.From == url {
-			p.keyHook = key
-			return true
+func сatchHook(w http.ResponseWriter, r *http.Request) {
+	url := "http://" + r.Host + r.URL.String()
+	success := "false"
+	hook := params.getHook(url)
+	if hook != nil {
+		body, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			writeLog("logs/error.log", err.Error())
+		}
+		secret, _ := os.LookupEnv("SECRET_TOKEN")
+		if !IsValidPayload(secret, r.Header.Get("X-Hub-Signature"), body) {
+			writeLog("logs/error.log", "Payload did not come from GitHub")
+			return
+		}
+		switch hook.getType() {
+		case "http":
+			respBody := bytes.NewReader(body)
+			client := &http.Client{}
+			req, err := http.NewRequest(r.Method, hook.getToUrl(), respBody)
+			if err != nil {
+				writeLog("logs/error.log", err.Error())
+			}
+			req.Header = r.Header
+			req.PostForm = r.PostForm
+			resp, err := client.Do(req)
+			if err != nil {
+				writeLog("logs/error.log", err.Error())
+			}
+			if resp.StatusCode == http.StatusOK {
+				success = "true"
+			}
+			w.WriteHeader(resp.StatusCode)
+		case "ssh":
+			pwd, err := os.Getwd()
+			if err != nil {
+				writeLog("logs/error.log", err.Error())
+			}
+			from := pwd + "/request_body.json"
+			to := hook.getBaseDir() + hook.getRequestBodyPath()
+			writeFile(from, body)
+			moveFileOnSsh(hook, from, to)
+			status := RunSsh(hook)
+			if status == http.StatusOK {
+				success = "true"
+			}
+			w.WriteHeader(status)
+		case "local":
+			writeFile(os.Getenv("cis_base_dir")+"/"+hook.getRequestBodyPath(), body)
+			status := RunLocal(hook)
+			if status == http.StatusOK {
+				success = "true"
+			}
+			w.WriteHeader(status)
 		}
 	}
-	return false
+	str := "success:[" + success + "] method:[" + r.Method + "] url:[" + url + "] " + fmt.Sprintln(r.Header)
+	writeLog("logs/http.log", str[:len(str)-1])
 }
 
-func (p Params) getEnvVars() []string {
-	from := p.Hooks[p.keyHook].From
-	result := make([]string, 0)
-	result = append(result,
-		"webhook_query_string="+from,
-		"webhook_event_type="+p.eventType,
-		"webhook_request_body=$cis_base_dir"+p.getRequestBodyPath())
-	args := strings.Split(from[strings.Index(from, "?")+1:], "&")
-	return append(result, args...)
+func RunLocal(h *hook) int {
+	envVars := make([]string, 0)
+	envVars = append(envVars, readEnvVarsFromCis(h)...)
+	envVars = append(envVars, getEnvVars(h)...)
+	command := h.getBaseDir() + "/core/" + getStartJob(envVars)
+	cmd := exec.Command(command, h.getJob())
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+	cmd.Env = append(os.Environ(), envVars...)
+	err := cmd.Run()
+	if err != nil {
+		writeLog("logs/error.log", err.Error())
+	}
+	out := buf.String()[0 : len(buf.String())-1]
+	fmt.Println(out)
+	if strings.Index(out, "Exit code: 0") != -1 {
+		return http.StatusOK
+	}
+	return http.StatusBadRequest
 }
 
-func (p Params) readEnvVarsFromCis() []string {
+func getStartJob(envVars []string) string {
+	for _, envVar := range envVars {
+		if strings.HasPrefix(envVar, "startjob") {
+			return envVar[strings.Index(envVar, "=")+1:]
+		}
+	}
+	return ""
+}
+
+func getOutOfSshCommand(h *hook, command string, envVars ...string) string {
+	session, err := getSshSession(h)
+	if err != nil {
+		writeLog("logs/error.log", err.Error())
+	}
+	defer session.Close()
+	for _, envVar := range envVars {
+		envAsArr := strings.Split(envVar, "=")
+		err := session.Setenv(envAsArr[0], envAsArr[1])
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	b, err := session.CombinedOutput(command)
+	if err != nil {
+		writeLog("logs/error.log", err.Error())
+	}
+	out := string(b)
+	if out[len(out)-1:] == "\n" {
+		return out[:len(out)-1]
+	} else {
+		return out
+	}
+}
+
+func readEnvVarsFromCisOnSsh(h *hook) []string {
 	result := make([]string, 0)
-	cisEnv, err := p.readConfFile("/core/cis.conf")
+	cisEnv := strings.Split(getOutOfSshCommand(h, "cat "+h.getBaseDir()+"/core/cis.conf"), "\n")
+	result = append(result, cisEnv...)
+	project := h.getJob()[:strings.Index(h.getJob(), "/")]
+	jobEnv := strings.Split(getOutOfSshCommand(h, "cat "+h.getBaseDir()+"/jobs/"+project+"/job.conf"), "\n")
+	result = append(result, jobEnv...)
+	return result
+}
+
+func readEnvVarsFromCis(h *hook) []string {
+	result := make([]string, 0)
+	cisEnv, err := readConfFile("/core/cis.conf")
 	if err != nil {
 		writeLog("logs/error.log", err.Error())
 	}
 	result = append(result, cisEnv...)
-	jobEnv, err := p.readConfFile("/jobs/internal/" + p.getJobName() + "/job.conf")
+	project := h.getJob()[:strings.Index(h.getJob(), "/")]
+	jobEnv, err := readConfFile("/jobs/" + project + "/job.conf")
 	if err != nil {
 		writeLog("logs/error.log", err.Error())
 	}
@@ -101,7 +279,7 @@ func (p Params) readEnvVarsFromCis() []string {
 	return result
 }
 
-func (p Params) readConfFile(fileName string) ([]string, error) {
+func readConfFile(fileName string) ([]string, error) {
 	file, err := os.Open(os.Getenv("cis_base_dir") + fileName)
 	if err != nil {
 		return nil, err
@@ -116,242 +294,42 @@ func (p Params) readConfFile(fileName string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
-func (p Params) readEnvVarsFromCisOnSsh() []string {
+func getEnvVars(h *hook) []string {
+	from := h.getFromUrl()
 	result := make([]string, 0)
-	base_dir := getOutOfSshCommand(p, "echo $cis_base_dir")
-	cisEnv := strings.Split(getOutOfSshCommand(p, "cat "+base_dir+"/core/cis.conf"), "\n")
-	result = append(result, cisEnv...)
-	jobEnv := strings.Split(getOutOfSshCommand(p, "cat "+base_dir+"/jobs/internal/"+p.getJobName()+"/job.conf"), "\n")
-	result = append(result, jobEnv...)
-	return result
+	result = append(result,
+		"webhook_query_string="+from,
+		"webhook_event_type="+getEventType(),
+		"webhook_request_body="+h.getBaseDir()+h.getRequestBodyPath())
+	args := strings.Split(from[strings.Index(from, "?")+1:], "&")
+	return append(result, args...)
 }
 
-type SshParams struct {
-	User   string
-	Host   string
-	Port   string
-	Launch string
+func getEventType() string {
+	return ""
 }
 
-func getSshParams(cmd string) *SshParams {
-	result := &SshParams{}
-	params := strings.Split(cmd, " ")
-	for i, param := range params {
-		if strings.Contains(param, "@") {
-			temp := strings.Split(param, "@")
-			result.User = temp[0]
-			result.Host = temp[1]
-		} else if strings.Compare(strings.ToLower(param), "-p") == 0 {
-			result.Port = params[i+1]
-		} else if i == len(params)-2 {
-			result.Launch = param + " " + params[i+1]
-		}
-	}
-	return result
-}
-
-func IsValidPayload(secret, headerHash string, payload []byte) bool {
-	hash := "sha1=" + HashPayload(secret, payload)
-	return hmac.Equal(
-		[]byte(hash),
-		[]byte(headerHash),
-	)
-}
-
-func HashPayload(secret string, playloadBody []byte) string {
-	hm := hmac.New(sha1.New, []byte(secret))
-	hm.Write(playloadBody)
-	sum := hm.Sum(nil)
-	return fmt.Sprintf("%x", sum)
-}
-
-func RunLocal(p Params) int {
+func RunSsh(h *hook) int {
 	envVars := make([]string, 0)
-	envVars = append(envVars, p.readEnvVarsFromCis()...)
-	envVars = append(envVars, p.getEnvVars()...)
-	command := strings.Split(p.getHookTo(), " ")
-	path := buildPath(command[0], envVars)
-	cmd := exec.Command(path, command[1:]...)
-	var buf bytes.Buffer
-	cmd.Stdout = &buf
-	cmd.Env = append(os.Environ(), envVars...)
-	err := cmd.Run()
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	out := buf.String()[0 : len(buf.String())-1]
+	envVars = append(envVars, readEnvVarsFromCisOnSsh(h)...)
+	envVars = append(envVars, getEnvVars(h)...)
+	out := getOutOfSshCommand(h, h.getBaseDir()+"core/$startjob "+h.getJob(), envVars...)
 	if strings.Index(out, "Exit code: 0") != -1 {
 		return http.StatusOK
 	}
 	return http.StatusBadRequest
 }
 
-func getClientConfig(userName string) *ssh.ClientConfig {
-	key, err := ioutil.ReadFile(os.Getenv("HOME") + "/.ssh/id_rsa")
+func getParams() (out Params) {
+	jsonData, err := ioutil.ReadFile("config.json")
 	if err != nil {
 		writeLog("logs/error.log", err.Error())
 	}
-	signer, err := ssh.ParsePrivateKey(key)
+	err = json.Unmarshal(jsonData, &out)
 	if err != nil {
 		writeLog("logs/error.log", err.Error())
 	}
-	hostKeyCallback, err := knownhosts.New(os.Getenv("HOME") + "/.ssh/known_hosts")
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	return &ssh.ClientConfig{
-		User: userName,
-		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
-		},
-		HostKeyCallback: hostKeyCallback,
-	}
-}
-
-func buildPath(path string, envVars []string) string {
-	for _, envVar := range envVars {
-		if strings.HasPrefix(envVar, "startjob") {
-			os.Setenv("startjob", envVar[strings.Index(envVar, "=")+1:])
-			break
-		}
-	}
-	pathAsArr := strings.Split(path, "/")
-	for key, item := range pathAsArr {
-		if strings.HasPrefix(item, "$") {
-			pathAsArr[key] = os.Getenv(item[1:])
-		}
-	}
-	return strings.Join(pathAsArr, "/")
-}
-
-func RunSsh(p Params) int {
-	sshParams := getSshParams(p.getHookTo())
-	client, err := ssh.Dial("tcp", sshParams.Host+":"+sshParams.Port, getClientConfig(sshParams.User))
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	session, err := client.NewSession()
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	defer session.Close()
-	envVars := make([]string, 0)
-	envVars = append(envVars, p.readEnvVarsFromCisOnSsh()...)
-	envVars = append(envVars, p.getEnvVars()...)
-
-	for _, envVar := range envVars {
-		envAsArr := strings.Split(envVar, "=")
-		err := session.Setenv(envAsArr[0], envAsArr[1])
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-	}
-	b, err := session.CombinedOutput(sshParams.Launch)
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	if strings.Index(string(b), "Exit code: 0") != -1 {
-		return http.StatusOK
-	}
-	return http.StatusBadRequest
-}
-
-func getOutOfSshCommand(p Params, command string) string {
-	sshParams := getSshParams(p.getHookTo())
-	client, err := ssh.Dial("tcp", sshParams.Host+":"+sshParams.Port, getClientConfig(sshParams.User))
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	session, err := client.NewSession()
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	defer session.Close()
-	b, err := session.CombinedOutput(command)
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	out := string(b)
-	if out[len(out)-1:] == "\n" {
-		return out[:len(out)-1]
-	} else {
-		return out
-	}
-}
-
-func moveFileOnSsh(p Params, from, to string) {
-	sshParams := getSshParams(p.getHookTo())
-	client, err := ssh.Dial("tcp", sshParams.Host+":"+sshParams.Port, getClientConfig(sshParams.User))
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	session, err := client.NewSession()
-	defer session.Close()
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	err = scp.CopyPath(from, to, session)
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-	err = os.Remove(from)
-	if err != nil {
-		writeLog("logs/error.log", err.Error())
-	}
-}
-
-func сatchHook(w http.ResponseWriter, r *http.Request) {
-	url := "http://" + r.Host + r.URL.String()
-	success := "false"
-	if params.setKeyHook(url) {
-		body, err := ioutil.ReadAll(r.Body)
-		defer r.Body.Close()
-		if err != nil {
-			writeLog("logs/error.log", err.Error())
-		}
-		secret, _ := os.LookupEnv("SECRET_TOKEN")
-		if !IsValidPayload(secret, r.Header.Get("X-Hub-Signature"), body) {
-			writeLog("logs/error.log", "Payload did not come from GitHub")
-			return
-		}
-		if strings.HasPrefix(params.getHookTo(), "http") {
-			client := &http.Client{}
-			req, err := http.NewRequest(r.Method, params.getHookTo(), r.Body)
-			if err != nil {
-				writeLog("logs/error.log", err.Error())
-			}
-			req.Header = r.Header
-			req.PostForm = r.PostForm
-			resp, err := client.Do(req)
-			if err != nil {
-				writeLog("logs/error.log", err.Error())
-			}
-			if resp.StatusCode == http.StatusOK {
-				success = "true"
-			}
-			w.WriteHeader(resp.StatusCode)
-		} else if strings.HasPrefix(params.getHookTo(), "ssh") {
-			from := params.Root + "request_body.json"
-			to := getOutOfSshCommand(params, "echo $cis_base_dir") + params.getRequestBodyPath()
-			writeFile(from, body)
-			moveFileOnSsh(params, from, to)
-			writeFile(os.Getenv("cis_base_dir")+params.getRequestBodyPath(), body)
-			status := RunSsh(params)
-			if status == http.StatusOK {
-				success = "true"
-			}
-			w.WriteHeader(status)
-		} else {
-			writeFile(os.Getenv("cis_base_dir")+params.getRequestBodyPath(), body)
-			status := RunLocal(params)
-			if status == http.StatusOK {
-				success = "true"
-			}
-			w.WriteHeader(status)
-		}
-	}
-	str := "success:[" + success + "] method:[" + r.Method + "] url:[" + url + "] " + fmt.Sprintln(r.Header)
-	writeLog("logs/http.log", str[:len(str)-1])
+	return out
 }
 
 func writeLog(fileName string, str string) {
@@ -371,6 +349,21 @@ func writeFile(fileName string, data []byte) {
 	}
 	defer file.Close()
 	file.Write(data)
+}
+
+func IsValidPayload(secret, headerHash string, payload []byte) bool {
+	hash := "sha1=" + HashPayload(secret, payload)
+	return hmac.Equal(
+		[]byte(hash),
+		[]byte(headerHash),
+	)
+}
+
+func HashPayload(secret string, playloadBody []byte) string {
+	hm := hmac.New(sha1.New, []byte(secret))
+	hm.Write(playloadBody)
+	sum := hm.Sum(nil)
+	return fmt.Sprintf("%x", sum)
 }
 
 func main() {
